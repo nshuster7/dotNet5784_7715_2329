bd1e0505507feae338135240e7558e365ca59bb5
EfratAharoni <efaharon@g.jct.ac.il>
2024-01-09 19:38:07 +0200

    This is our second reservoir, we had our first reservoir and we did stage 0. However, we had a problem and had to open a new repository with the project starting from stage 1

---
+++ Stage0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Stage0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7715();
+        }
+
+        private static void Welcome7715()
+        {
+            Console.WriteLine("Enter your name");
+        }
+    }
+}
