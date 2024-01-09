f5a76f3595de31d47480c37c705bc0cdbf78855d
EfratAharoni <efaharon@g.jct.ac.il>
2024-01-09 19:48:54 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program7715.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7715();
+            Welcome2329();
+            Console.ReadKey();
+        }
+        static partial void Welcome2329();
+        private static void Welcome7715()
+        {
+            Console.WriteLine("Enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console aplication", userName);
+        }
+    }
+}
