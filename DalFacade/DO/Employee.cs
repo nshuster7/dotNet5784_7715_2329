984acf212d023887541b3ef3749e3c90ee352a74
EfratAharoni <efaharon@g.jct.ac.il>
2024-01-09 21:49:25 +0200

    B.H. I completed step 3 through 3.c, adding only the Manager and Employee entity (no comments, note). Check that everything is working as it should.

---
+++ DalFacade/DO/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace DO;
+public record Employee
+(
+    int Id,
+    string Name,
+    string Email,
+    Specialization type,
+    decimal HourlyRate,
+    bool IsOnShift = false,
+    DateTime? ShiftStartTime = null,
+    DateTime? ShiftEndTime = null
+)
+{
+    public Employee() : this(0, "", "" , 0) { } // Empty constructor for stage 3
+}
+
+public enum Specialization
+{
+
+}
