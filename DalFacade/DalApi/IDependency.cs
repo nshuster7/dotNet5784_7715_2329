b5523ace6de158cab084914a9eb232d07ffd3d5b
EfratAharoni <efaharon@g.jct.ac.il>
2024-01-15 14:26:13 +0200

    Try running the program to see that everything is still

--- DalFacade/DalApi/IDependency.cs
+++ DalFacade/DalApi/IDependency.cs
@@ -1,14 +1,3 @@
-﻿
-
-
-namespace DalApi;
+﻿namespace DalApi;
 using DO;
-public interface IDependency
-{
-    int Create(Dependency item); //Creates new entity object in DAL
-    Dependency? Read(int id); //Reads entity object by its ID 
-    Dependency? Read(int t1 ,int t2);
-    List<Dependency> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Dependency item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-}
+public interface IDependency : ICrud<Dependency> { }
--- DalFacade/DalApi/IEmployee.cs
+++ DalFacade/DalApi/IEmployee.cs
@@ -1,11 +1,3 @@
-﻿
-namespace DalApi;
+﻿namespace DalApi;
 using DO;
-public interface IEmployee
-{
-    int Create(Employee item); //Creates new entity object in DAL
-    Employee? Read(int id); //Reads entity object by its ID 
-    List<Employee> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Employee item); //Updates entity object
-    void Delete(int id); //Deletes an object by its Id
-}
+public interface IEmployee : ICrud<Employee> { }
