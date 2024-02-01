da1c3d9a0bb3f38c943c3680b92d3834b340b3c9
EfratAharoni <efaharon@g.jct.ac.il>
2024-02-01 19:06:47 +0200

    left: 8,12,13

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,35 @@
+﻿namespace BO;
+
+[Serializable]
+public class BlDoesNotExistException : Exception
+{
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException)
+            : base(message, innerException) { }
+}
+
+[Serializable]
+public class BlNullPropertyException : Exception
+{
+    public BlNullPropertyException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlAlreadyExistsException : Exception
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlDeletionImpossible : Exception
+{
+    public BlDeletionImpossible(string? message) : base(message) { }
+}
+public class BlWrongValueException : Exception
+{
+    public BlWrongValueException(string? message) : base(message) { }
+}
+//public class DalXMLFileLoadCreateException : Exception
+//{
+//    public DalXMLFileLoadCreateException(string? message) : base(message) { }
+//}
