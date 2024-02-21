6032ff3d887eeb0877cb98a08e9067d5199661fb
EfratAharoni <efaharon@g.jct.ac.il>
2024-02-21 23:00:46 +0200

    Add a Single Item View window - Design controls

--- PL/Employee/EmployeeListWindow.xaml.cs
+++ PL/Employee/EmployeeListWindow.xaml.cs
@@ -22,24 +22,23 @@ public partial class EmployeeListWindow : Window
     public EmployeeListWindow()
     {
         InitializeComponent();
-        EmployeeList = s_bl?.Employee.ReadAll()!;
+        EmployeeList = s_bl.Employee.ReadAll();
     }
     static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-    public IEnumerable<BO.EmployeeInTask> EmployeeList
+    public IEnumerable<BO.Employee> EmployeeList
     {
-        get { return (IEnumerable<BO.EmployeeInTask>)GetValue(EmployeeListProperty); }
+        get { return (IEnumerable<BO.Employee>)GetValue(EmployeeListProperty); }
         set { SetValue(EmployeeListProperty, value); }
     }
 
     public static readonly DependencyProperty EmployeeListProperty =
-        DependencyProperty.Register("EmployeeList", typeof(IEnumerable<BO.EmployeeInTask>), typeof(EmployeeListWindow), new PropertyMetadata(null));
+        DependencyProperty.Register("EmployeeList", typeof(IEnumerable<BO.Employee>), typeof(EmployeeListWindow), new PropertyMetadata(null));
 
     public BO.Type Type { get; set; } = BO.Type.All;
 
     private void WorkerListSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-       EmployeeList  = (Type == BO.Type.All) ?
-    s_bl?.Employee.ReadAll()! : s_bl?.Employee.ReadAll(item => (BO.Type)item.Type! == Type)!;
-
+        EmployeeList = (Type == BO.Type.All) ?
+            s_bl?.Employee.ReadAll()! : s_bl?.Employee.ReadAll(item => (BO.Type)item.Type! == Type)!;
     }
 }
--- PL/Enums.cs
+++ PL/Enums.cs
@@ -14,4 +14,11 @@ internal class EmployeeType : IEnumerable
 
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
+internal class WorkStatus : IEnumerable
+{
+    static readonly IEnumerable<BO.WorkStatus> s_enums =
+(Enum.GetValues(typeof(BO.WorkStatus)) as IEnumerable<BO.WorkStatus>)!;
+
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
 
